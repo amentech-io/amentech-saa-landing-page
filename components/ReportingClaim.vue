cb33a584747a19c7abe93350c9c260c867ce50ea
zirbest <zirbst@gmail.com>
2023-08-28 15:05:35 +0100

    chore(reporting-claim): ui

--- components/ReportingClaim.vue
+++ components/ReportingClaim.vue
@@ -24,23 +24,30 @@ const cards = [
       <div class="grid 2xl:grid-cols-2">
         <div class="relative my-a hidden 2xl:block 2xl:pr-14">
           <img src="/reporting-claim-bg.png" alt="phone app">
-          <div class="absolute right-1/2 top-1/2 aspect-1 w-3/4 translate-x-1/2 rounded-full bg-[#BEBBD1]/40 blur-2xl -z-1 -translate-y-1/2" />
+          <div class="absolute right-1/2 top-1/2 aspect-1 w-3/4 translate-x-1/2 rounded-full bg-[#FC5185]/25 blur-250 -z-1 -translate-y-1/2" />
         </div>
 
         <div>
-          <h3 class="mx-auto max-w-20.8ch px-6 text-lg font-bold lg:text-5xl" v-html="t('title')" />
+          <h3 class="max-w-18ch text-lg font-bold lg:text-5xl" v-html="t('title')" />
 
-          <p class="mx-auto mt-3 max-w-68ch text-center text-xs font-medium lg:mt-10 lg:text-2xl" v-html="t('subtitle')" />
+          <p class="mx-auto mt-3 max-w-68ch text-center text-xs font-medium lg:mt-10 lg:text-left lg:text-2xl" v-html="t('subtitle')" />
 
-          <div class="mt-5 flex flex-wrap justify-center gap-4 lg:mt-22 lg:flex-row 2xl:flex-nowrap lg:gap-5 2xl:pl-0">
+          <div class="mx-1 mt-5 flex flex-wrap justify-center gap-4 lg:mt-22 lg:flex-row 2xl:flex-nowrap lg:gap-5 2xl:pl-0">
             <div
               v-for="card, idx in cards" :key="idx"
-              class="relative rounded-xl bg-white px-5 py-4 lg:px-8 lg:py-6"
+              class="relative rounded-xl px-5 py-4 lg:px-8 lg:py-6"
             >
-              <div
-                class="absolute inset--1 z--1 border-4 border-white rounded-[17px] border-dashed bg-[length:100%_110%] from-[#1C57BC] via-[#9D5CA2] to-[rgba(252,81,133,0.94)] from-0% to-98% via-47% bg-gradient-to-b"
-              />
-              <span class="inline-block h-4 w-4 flex items-center justify-center rounded-2 bg-blue-200 text-xs font-bold text-blue-800 lg:h-6 lg:w-6">{{ idx + 1 }}</span>
+              <div class="absolute inset-px z--2 rounded-3 bg-[#05031F]" />
+              <div v-if="idx === 0" class="absolute inset-0 z--3 rounded-3 bg-[#F85186]" />
+              <div v-if="idx === 0" class="absolute inset-px z--1 rounded-3 from-[#F85186]/30 to-[#F55187]/10 from-0% to-100% bg-gradient-to-bl" />
+
+              <div v-if="idx === 1" class="absolute inset-0 z--3 rounded-3 bg-[#1C57BC]" />
+              <div v-if="idx === 1" class="absolute inset-px z--1 rounded-3 from-[#1C57BC]/30 to-[#1C57BC]/10 from-0% to-100% bg-gradient-to-bl" />
+
+              <div v-if="idx === 2" class="absolute inset-0 z--3 rounded-3 bg-[#FFFFFF]/30" />
+              <div v-if="idx === 2" class="absolute inset-px z--1 rounded-3 from-[#FFFFFF]/10 to-[#FFFFFF]/5 from-0% to-100% bg-gradient-to-bl" />
+
+              <span class="inline-block h-4 w-4 flex items-center justify-center rounded-2 bg-[#1C57BC]/20 text-xs font-extrabold text-white lg:h-10 lg:w-10 lg:text-xl">{{ idx + 1 }}</span>
               <p class="mt-4 max-w-12ch text-sm font-bold lg:mt-6 lg:text-2xl">
                 {{ card.title }}
               </p>
@@ -57,8 +64,8 @@ const cards = [
 
 <i18n lang="yaml">
 fr:
-  title: Déclarer un <span class="underline-gradient">sinistre</span>, c’est aussi simple que ça!
-  subtitle: <span class="text-[#1C57BC]">97%</span> de nos clients restent chez SAA après une gestion de sinistres
+  title: Déclarer un <span class="border rounded-xl border-[#EE4E80] px-2 pb-1px leading-10">sinistre</span>, c’est aussi simple que ça!
+  subtitle: Un sinistre ? Nous Sommes Là pour Vous. Une déclaration facile et une prise en charge rapide !
   item1:
     title: Que s’est-il passé ?
     description: Envoyez-nous vos photos et vos documents sur l’appli.
