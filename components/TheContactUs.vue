e071851ee228b6b5dfb15d5963444e4a144a95b9
zirbest <zirbst@gmail.com>
2023-09-11 13:38:01 +0100

    chore(contact-ui): update the UI

--- components/TheContactUs.vue
+++ components/TheContactUs.vue
@@ -37,15 +37,21 @@ async function onSubmit(v, { resetForm }) {
 </script>
 
 <template>
-  <div id="contact-us" class="pt-8">
-    <div class="m-auto px-6 py-8 container">
+  <div id="contact-us" class="overflow-hidden pt-8">
+    <div class="relative m-auto px-6 py-8 container">
+      <div class="absolute bottom-0 right-0 mr-20">
+        <img src="/contact-mail-icon.svg" alt="contact mail">
+      </div>
       <div class="flex flex-col">
         <h4
           class="text-center text-base font-extrabold uppercase md:mt-24 lg:text-9"
         >
           {{ $t('contact-us') }}
         </h4>
-        <p class="mx-auto mt-4 max-w-70ch text-center text-xs lg:mt-13 [&_b]:block lg:text-2xl lg:leading-10" v-html="t('subtitle', { email: 'contact@amentech.dz' })" />
+        <div class="grid mx-auto mt-4 max-w-80ch gap-8 text-center text-xs lg:mt-13 [&_b]:block lg:text-2xl lg:leading-10">
+          <p>{{ t('subtitle1') }}</p>
+          <p>{{ t('subtitle2') }}</p>
+        </div>
       </div>
 
       <Form
@@ -54,12 +60,17 @@ async function onSubmit(v, { resetForm }) {
         :validation-schema="fieldSchema"
         @submit="onSubmit"
       >
+        <div class="absolute left-0 top-0 translate-x--2/3 translate-y--1/3">
+          <img src="/contact-location-icon.svg" alt="contact location">
+        </div>
+
         <div class="absolute right-0 aspect-1 w-1/2 translate-x-1/2 rounded-full bg-[#FC5185]/25 blur-300 -translate-y-1/2" />
+
         <div class="flex gap-8">
           <div class="flex flex-grow flex-col space-y-2">
             <label for="fullName" class="text-xs font-medium lg:text-5">{{ $t('full-name') }} *</label>
             <div
-              class="of-hidden border border-[#181B34] rounded-lg text-black/80 hover:border-[#181B34]/60"
+              class="of-hidden border border-[#181B34] rounded-2xl text-black/80 hover:border-[#181B34]/60"
               :class="{ 'ring ring-red': fieldErrors.errors.fullName }"
             >
               <Field
@@ -72,7 +83,7 @@ async function onSubmit(v, { resetForm }) {
           <div class="flex flex-grow flex-col space-y-2">
             <label for="email" class="text-xs font-medium lg:text-5">E-Mail *</label>
             <div
-              class="of-hidden border border-[#181B34] rounded-lg text-black/80 hover:border-[#i81B34]/60"
+              class="of-hidden border border-[#181B34] rounded-2xl text-black/80 hover:border-[#i81B34]/60"
               :class="{ 'ring ring-red': fieldErrors.errors.email }"
             >
               <Field id="email" name="email" class="w-full px-3 py-4 text-xs outline-none lg:text-base" type="text" />
@@ -83,7 +94,7 @@ async function onSubmit(v, { resetForm }) {
         <div class="flex flex-col space-y-2">
           <label for="object" class="text-xs font-medium lg:text-5">{{ $t('subject') }} *</label>
           <div
-            class="of-hidden border border-[#181B34] rounded-lg text-black/80 hover:border-[#181B34]/60"
+            class="of-hidden border border-[#181B34] rounded-2xl text-black/80 hover:border-[#181B34]/60"
             :class="{ 'ring ring-red': fieldErrors.errors.object }"
           >
             <Field id="object" name="object" class="w-full px-3 py-4 text-xs outline-none lg:text-base" type="text" />
@@ -93,7 +104,7 @@ async function onSubmit(v, { resetForm }) {
         <div class="flex flex-col space-y-2">
           <label for="phone" class="text-xs font-medium lg:text-5">{{ $t('phone') }} *</label>
           <div
-            class="of-hidden border border-[#181B34] rounded-lg text-black/80 hover:border-[#181B34]/60"
+            class="of-hidden border border-[#181B34] rounded-2xl text-black/80 hover:border-[#181B34]/60"
             :class="{ 'ring ring-red': fieldErrors.errors.phone }"
           >
             <Field id="phone" name="phone" class="w-full px-3 py-4 text-xs outline-none lg:text-base" type="text" />
@@ -108,22 +119,26 @@ async function onSubmit(v, { resetForm }) {
             name="body"
             type="text"
             rows="3"
-            class="h-full w-full resize-none border border-[#181B34] rounded p-2 px-3 py-4 text-xs text-black/80 outline-none hover:border-[#181B34]/60 lg:rounded-md lg:text-base"
+            class="h-full w-full resize-none border border-[#181B34] rounded-2xl p-2 px-3 py-4 text-xs text-black/80 outline-none hover:border-[#181B34]/60 lg:text-base"
             :class="{ 'ring ring-red': fieldErrors.errors.body }"
           />
         </div>
 
         <div class="flex gap-6">
           <!-- <img src="/api/captcha" alt=""> -->
-          <img :src="captcha" alt="" class="cursor-pointer rounded-lg bg-white object-contain px-4" @click="getCaptcha()">
-          <div class="of-hidden border border-[#181B34] rounded-lg text-black/80 hover:border-[#181B34]/60">
+          <img
+            :src="captcha" alt="captcha"
+            class="cursor-pointer rounded-2xl w-38 bg-white object-contain px-4"
+            @click="getCaptcha()"
+          >
+          <div class="of-hidden border border-[#181B34] rounded-2xl text-black/80 hover:border-[#181B34]/60">
             <Field id="captcha" name="captcha" class="w-full px-3 py-4 text-xs outline-none lg:text-base" type="text" />
           </div>
         </div>
 
         <div class="flex justify-end pt-3 lg:pt-7">
           <button
-            class="inline-block w-full rounded-3 from-[#1C57BC] via-[#9D5CA2] to-[rgba(252,81,133,0.94)] from-0% to-98% via-47% bg-gradient-to-l px-10 py-2 font-semibold text-white transition lg:px-18 lg:py-5 lg:text-lg hover:opacity-90 hover:ring-4 hover:ring-white/50"
+            class="inline-block w-full rounded-2xl from-[#1C57BC] via-[#9D5CA2] to-[rgba(252,81,133,0.94)] from-0% to-98% via-47% bg-gradient-to-l px-10 py-2 font-semibold text-white transition lg:px-18 lg:py-5 lg:text-lg hover:opacity-90 hover:ring-4 hover:ring-white/50"
           >
             {{ $t('send') }}
           </button>
@@ -136,5 +151,6 @@ async function onSubmit(v, { resetForm }) {
 <i18n lang="yaml">
 fr:
   title: Une protection optimale
-  subtitle: Intéressé pour découvrir nos produits et services à travers une démonstration, ou pour résoudre vos problémes bien spécifiques, contactez-nous sur <b>{email}</b> ou envoyez     nous vos informations en remplissant la grille suivante
+  subtitle1: Nos équipes sont là pour répondre à tous vos besoins, que ce soit pour discuter de vos préoccupations, obtenir des informations détaillées sur nos produits,
+  subtitle2: faire une réclamation ou bénéficier d'un service client dévoué qui vous accompagnera tout au long de votre parcours vers une protection complète et adaptée à votre vie
 </i18n>
