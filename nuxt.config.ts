4f6c53a27005096f91e62bdca3b37b4ae42dc1e5
zirbest <zirbst@gmail.com>
2023-10-16 22:28:14 +0100

    chore(i18n): remove disable English

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,7 +26,7 @@ export default defineNuxtConfig({
     lazy: true,
     locales: [
       { code: 'fr', iso: 'fr-DZ', file: 'fr.yml', name: 'Français' },
-      { code: 'en', iso: 'en-DZ', file: 'en.yml', name: 'English' },
+      // { code: 'en', iso: 'en-DZ', file: 'en.yml', name: 'English' },
       { code: 'ar', iso: 'ar-DZ', file: 'ar.yml', name: 'العربية', dir: 'rtl' },
     ],
     defaultLocale: 'fr',
